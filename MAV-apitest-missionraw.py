b82f739706e0484540659e10027f208e80243ace
Federico Pratissoli <pratissolifederico@gmail.com>
2023-11-16 18:19:23 +0100

    Add mission progress tracking

--- MAV-apitest-missionraw.py
+++ MAV-apitest-missionraw.py
@@ -14,6 +14,9 @@ async def run():
             print("-- Connected to drone!")
             break
 
+    print_mission_progress_task = asyncio.ensure_future(
+        print_mission_progress(drone))
+    
     mission_items = []
 
     mission_items.append(mission_raw.MissionItem(
@@ -45,6 +48,33 @@ async def run():
     print("-- Uploading mission")
     await drone.mission_raw.upload_mission(mission_items)
     print("-- Done")
+
+    print("Waiting for drone to have a global position estimate...")
+    async for health in drone.telemetry.health():
+        if health.is_global_position_ok and health.is_home_position_ok:
+            print("-- Global position estimate OK")
+            break
+
+    print("-- Arming")
+    await drone.action.arm()
+
+    print("-- Starting mission")
+    await drone.mission.start_mission()
+
+    async for mission_progress in drone.mission_raw.mission_progress():
+        if mission_progress.current >= mission_progress.total:
+            print("-- Mission completed")
+            break
+        else:
+            await asyncio.sleep(1)
+
+    #print_mission_progress_task.cancel()
+
+async def print_mission_progress(drone):
+    async for mission_progress in drone.mission_raw.mission_progress():
+        print(f"Mission progress: "
+              f"{mission_progress.current}/"
+              f"{mission_progress.total}")
 
 if __name__ == '__main__':
     asyncio.run(run())
