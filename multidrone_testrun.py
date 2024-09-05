f0cb1d1749204b2ae30a772236ad5bc005057224
Federico Pratissoli <pratissolifederico@gmail.com>
2024-09-06 01:11:41 +0200

    possible simulation with local coorinates, but not tested yet

--- multidrone_testrun.py
+++ multidrone_testrun.py
@@ -105,6 +105,28 @@ class DroneSwarm:
                                          latitude_deg=lat, longitude_deg=lon))
         await asyncio.gather(*tasks)
 
+    async def run_goto_local(self, local_coords):
+        #TODO: not tested
+        tasks = []
+        for drone, coords in zip(self.drones, local_coords):
+            x, y, z = coords
+            lat, lon, alt = local_to_global(x, y, z, self.origin_lat, self.origin_lon, self.origin_alt)
+            tasks.append(drone.run_goto(lat, lon, alt - self.origin_alt))  # alt is relative to origin
+        await asyncio.gather(*tasks)
+
+    async def run_orbit_formation_local(self, center_x, center_y, radius, altitude, num_drones):
+        #TODO: not tested
+        center_lat, center_lon, _ = local_to_global(center_x, center_y, 0, self.origin_lat, self.origin_lon, self.origin_alt)
+        tasks = []
+        for i, drone in enumerate(self.drones[:num_drones]):
+            angle = (2 * math.pi * i) / num_drones
+            x = center_x + (radius * math.cos(angle))
+            y = center_y + (radius * math.sin(angle))
+            lat, lon, _ = local_to_global(x, y, 0, self.origin_lat, self.origin_lon, self.origin_alt)
+            tasks.append(drone.run_orbit(radius=5, velocity_ms=2, relative_altitude=altitude, 
+                                         latitude_deg=lat, longitude_deg=lon))
+        await asyncio.gather(*tasks)
+
     async def monitor_swarm(self):
         #TODO: not tested
         monitoring_tasks = [drone._start_state_monitoring() for drone in self.alldrones]
@@ -127,11 +149,27 @@ async def run_swarm_mission(swarm):
         (47.397806, 8.543260, 30),
         # Add more coordinates for additional drones
     ]
+
     await swarm.run_goto_formation(formation_coords)
     await asyncio.sleep(30)
+
+    # Example formation flight using local coordinates
+    # local_formation_coords = [
+    #     (10, 0, 20),   # 10m east, 0m north, 20m up
+    #     (0, 10, 25),   # 0m east, 10m north, 25m up
+    #     (-10, 0, 30),  # 10m west, 0m north, 30m up
+    #     # Add more coordinates for additional drones
+    # ]
+
+    # await swarm.run_goto_local(local_formation_coords)
+    # await asyncio.sleep(30)
     
     # Example orbit formation
     #await swarm.run_orbit_formation(47.397606, 8.543060, 50, 30)
+    #await asyncio.sleep(60)
+
+    # Example orbit formation using local coordinates
+    #await swarm.run_orbit_formation_local(0, 0, 50, 30, len(swarm.drones))
     #await asyncio.sleep(60)
     
     await swarm.return_swarm_to_launch()
